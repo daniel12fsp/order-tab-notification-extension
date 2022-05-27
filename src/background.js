5a9a7ba969f4e4ad81ea4828d8f34ffcd7c3a3e2
Daniel Pereira <daniel12fsp@yahoo.com.br>
2022-05-27 16:34:51 -0400

    Initial files

---
+++ src/background.js
@@ -0,0 +1,18 @@
+console.log("background.js");
+
+const store = {
+  notifications: [],
+};
+
+// function handleMessage(request, sender, sendResponse) {
+//     console.log("background", {request, sender, sendResponse});
+//   }
+
+// browser.runtime.onMessage.addListener(handleMessage);
+
+// function updateContent() {
+//   browser.tabs.query({currentWindow: true})
+//     .then((tabs) => {
+//       return console.log(tabs);
+//     })
+// }
---
+++ src/sidebar/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const NoPriorityTabs = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 0;
+`;
