9eadab6b301f32f17cb4be41c8b7024f7f6ee371
Daniel Pereira <daniel12fsp@yahoo.com.br>
2022-05-27 16:39:00 -0400

    add husky for pre-commit hook

---
+++ src/sidebar/components/TabList.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { List, ListItem } from "./List";
+
+export const NoPriorityTabs = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 0;
+`;
+
+function PriorityTabs({ priorityTabs, onClick }) {
+  if (priorityTabs.length === 0)
+    return <NoPriorityTabs>Sem tabs com prioridades</NoPriorityTabs>;
+  return (
+    <List>
+      {priorityTabs.map((tab) => (
+        <ListItem
+          key={tab.id}
+          icon={tab.favIconUrl}
+          title={tab.title}
+          subtitle={tab.url}
+          onClick={onClick(tab)}
+        ></ListItem>
+      ))}
+    </List>
+  );
+}
+export function TabList({ priorityTabs, onClick, normalTabs }) {
+  return <>
+    <PriorityTabs priorityTabs={priorityTabs} onClick={onClick} />
+    <hr />
+    <List>
+      {normalTabs.map((tab) => (
+        <ListItem
+          key={tab.id}
+          icon={tab.favIconUrl}
+          title={tab.title}
+          subtitle={tab.url}
+          onClick={onClick(tab)}
+        />
+      ))}
+    </List>
+  </>;
+}
--- src/sidebar/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const NoPriorityTabs = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 30px 0;
-`;
