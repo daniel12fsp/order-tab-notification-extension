9f23fb452fdf134117b22a0b9a15947de30f6474
Daniel Pereira <daniel12fsp@yahoo.com.br>
2022-06-15 17:38:49 -0400

    Add Tab more field in interface

--- src/sidebar/interface/Tab.interface.ts
+++ src/sidebar/interface/Tab.interface.ts
@@ -1,6 +1,36 @@
-interface ITab {
+export interface ITab {
   id: number;
-  title: string;
-  url: string;
+  index: number;
+  windowId: number;
+  highlighted: boolean;
   active: boolean;
+  attention: boolean;
+  pinned: boolean;
+  status: "complete" | "loading";
+  hidden: boolean;
+  discarded: boolean;
+  incognito: boolean;
+  width: number;
+  height: number;
+  lastAccessed: number;
+  audible: boolean;
+  mutedInfo: MutedInfo;
+  isArticle?: boolean;
+  isInReaderMode: boolean;
+  sharingState: SharingState;
+  successorTabId: number;
+  cookieStoreId: string;
+  url: string;
+  title: string;
+  favIconUrl: string;
+  openerTabId?: number;
+}
+
+export interface MutedInfo {
+  muted: boolean;
+}
+
+export interface SharingState {
+  camera: boolean;
+  microphone: boolean;
 }
